7c0fa16216785d9eac5459ae1c07eeaa14ad307f
rhit-mooretj <112582846+rhit-mooretj@users.noreply.github.com>
2026-02-05 16:12:59 -0500

    Framework setup, can open comment section

--- Chrome Extension/csse463_yt_backend/scripts/content.js
+++ Chrome Extension/csse463_yt_backend/scripts/content.js
@@ -18,3 +18,45 @@ observer.observe(document.body, {
     childList: true,
     subtree: true
 });
+
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.altKey && event.key === 'c') {
+        // Open comments section
+        console.log('Ctrl+Alt+C key combination pressed!');
+        const targetButton = document.querySelector('button[aria-label*="comments" i], button[aria-label*="Comment" i]'); // ID from youtube source code
+        targetButton?.click();
+    } else if (event.ctrlKey && event.altKey && event.key === 'l') {
+        // Like short
+        console.log('Ctrl+Alt+L key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'd') {
+        // Dislike short
+        console.log('Ctrl+Alt+D key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'x') {
+        // Close comment section
+        console.log('Ctrl+Alt+X key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'n') {
+        // Next Comment
+        console.log('Ctrl+Alt+N key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'p') {
+        // Previous Comment
+        console.log('Ctrl+Alt+P key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'b') {
+        // Select comment box
+        console.log('Ctrl+Alt+B key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 't') {
+        // Previous short
+        console.log('Ctrl+Alt+T key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'y') {
+        // Next short
+        console.log('Ctrl+Alt+Y key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'v') {
+        // Post Comment
+        console.log('Ctrl+Alt+V key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'q') {
+        // Like Comment
+        console.log('Ctrl+Alt+Q key combination pressed!');
+    } else if (event.ctrlKey && event.altKey && event.key === 'w') {
+        // Dislike Comment
+        console.log('Ctrl+Alt+W key combination pressed!');
+    }
+});
