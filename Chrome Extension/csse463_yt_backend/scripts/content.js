7ac97017640acd74e8638856c01cacde3ec9aa5f
rhit-mooretj <mooretj@rose-hulman.edu>
2026-02-05 20:27:58 -0500

    Major milestone on chrome extension

--- Chrome Extension/csse463_yt_backend/scripts/content.js
+++ Chrome Extension/csse463_yt_backend/scripts/content.js
@@ -23,8 +23,7 @@ document.addEventListener('keydown', function (event) {
     if (event.ctrlKey && event.altKey && event.key === 'c') {
         // Open comments section
         console.log('Ctrl+Alt+C key combination pressed!');
-        const targetButton = document.querySelector('button[aria-label*="comments" i], button[aria-label*="Comment" i]'); // ID from youtube source code
-        targetButton?.click();
+        document.querySelector('button[aria-label*="comments" i], button[aria-label*="Comment" i]')?.click(); // ID from youtube source code
     } else if (event.ctrlKey && event.altKey && event.key === 'l') {
         // Like short
         console.log('Ctrl+Alt+L key combination pressed!');
@@ -36,34 +35,43 @@ document.addEventListener('keydown', function (event) {
     } else if (event.ctrlKey && event.altKey && event.key === 'r') {
         // Remove like/dislike short
         console.log('Ctrl+Alt+R key combination pressed!');
-        document.querySelector('button[aria-label*="Dislike this video"]')?.click();
+        document.querySelector('button[aria-label*="Dislike this video"]')?.click(); // Disliking twice removes like or dislike
         document.querySelector('button[aria-label*="Dislike this video"]')?.click();
     } else if (event.ctrlKey && event.altKey && event.key === 'x') {
         // Close comment section
         console.log('Ctrl+Alt+X key combination pressed!');
+        document.querySelector('ytd-engagement-panel-section-list-renderer button[aria-label="Close"]')?.click();
     } else if (event.ctrlKey && event.altKey && event.key === 'n') {
         // Next Comment
+        // TODO: Implement scrolling to next comment
         console.log('Ctrl+Alt+N key combination pressed!');
     } else if (event.ctrlKey && event.altKey && event.key === 'p') {
         // Previous Comment
+        // TODO: Implement scrolling to previous comment
         console.log('Ctrl+Alt+P key combination pressed!');
     } else if (event.ctrlKey && event.altKey && event.key === 'b') {
         // Select comment box
         console.log('Ctrl+Alt+B key combination pressed!');
+        document.querySelector('#placeholder-area')?.click();
     } else if (event.ctrlKey && event.altKey && event.key === 't') {
         // Previous short
         console.log('Ctrl+Alt+T key combination pressed!');
+        document.querySelector('#navigation-button-up button')?.click();
     } else if (event.ctrlKey && event.altKey && event.key === 'y') {
         // Next short
         console.log('Ctrl+Alt+Y key combination pressed!');
+        document.querySelector('#navigation-button-down button')?.click();
     } else if (event.ctrlKey && event.altKey && event.key === 'v') {
         // Post Comment
         console.log('Ctrl+Alt+V key combination pressed!');
+        document.querySelector('ytd-button-renderer#submit-button button')?.click();
     } else if (event.ctrlKey && event.altKey && event.key === 'q') {
         // Like Comment
+        // TODO: Implement liking a comment
         console.log('Ctrl+Alt+Q key combination pressed!');
     } else if (event.ctrlKey && event.altKey && event.key === 'w') {
         // Dislike Comment
+        // TODO: Implement disliking a comment
         console.log('Ctrl+Alt+W key combination pressed!');
     }
 });
